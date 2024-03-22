2c2b9e81659915807280ed4d3a37b12c7c6675bd
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2024-03-22 19:17:44 +0530

    More debug added

--- plugins/filter_files.py
+++ plugins/filter_files.py
@@ -46,25 +46,34 @@ async def forward_cmd(bot, message):
         logger.info("got msgs")
     except:
         return await message.reply('Make Sure That Iam An Admin In The Channel, if channel is private')
+    logger.info("out from try block")
     if k.empty:
         return await message.reply('This may be group and iam not a admin of the group.')
+    logger.info("k is not empty")
     if lock.locked():
         return await message.reply_text('<b>Wait until previous process complete.</b>')
+    logger.info("its not locked")
     user = await db.get_user(int(message.from_user.id))
+    logger.info("got user")
     if user is not None:
+        logger.info("user is not None")
         if int(user['target_chat']) == 0:
             return await bot.send_message(
                 chat_id=message.from_user.id,
                 text="<b>Fist add your target channel ID using /set_target command !</b>"
             )
-        logger.info("got user")
+        logger.info("target chat is already set")
     else:
+        logger.info("user is none")
         await db.new_user(message.from_user.id, message.from_user.first_name, message.from_user.username)
+        logger.info("added user")
         return await message.reply_text(
             text="Hey, Add a target chat ID using /set_target command !"
         )
+    logger.info("user None checking done")
     await db.update_any(message.from_user.id, 'last_msg_id', f'{last_msg_id}')
     await db.update_any(message.from_user.id, 'source_chat', f'{source_chat_id}')
+    logger.info("db updated")
     temp_utils.UTILS[int(message.from_user.id)] = {
         'last_msg_id': int(last_msg_id),
         'source_chat_id': source_chat_id,
@@ -76,6 +85,7 @@ async def forward_cmd(bot, message):
     ],[
         InlineKeyboardButton("NO", callback_data="close")
     ]]
+    logger.info("btn generated")
     target_chat = await bot.get_chat(chat_id=int(user['target_chat']))
     logger.info("got target chat")
     await bot.send_message(
