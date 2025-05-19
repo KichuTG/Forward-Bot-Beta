bca196f9176860801ce60ed75ae1f16d6482b303
Kichu <kichumon333@outlook.com>
2025-05-20 00:44:24 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -19,4 +19,4 @@ PORT = int(environ.get("PORT", "8080"))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "https://github.com/Joelkb/File-Forward-Bot")
 DB_URI = environ.get('DB_URI', "")
-DB_NAME = environ.get('DB_NAME', "")
+DB_NAME = environ.get('DB_NAME', "frwdbot")
